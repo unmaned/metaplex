2d32bf5ca5cbe3407b828fb67491e4f281abf983
bharathreddza <bharathreddyza@gmail.com>
2022-02-19 09:21:36 +0530

    read me

--- js/packages/web/src/views/staticPage/staticData.js
+++ js/packages/web/src/views/staticPage/staticData.js
@@ -8,7 +8,7 @@ export const data = {
   midContent: {
     sections: [
       {
-        title: 'Metaplex is a protocol built on top of Solana that allows:',
+        title: 'betaplex is an opensource protocol built for all major chains that allows:',
         paragraphs: [
           '• Creating/Minting non-fungible tokens;',
           '• Starting a variety of auctions for primary/secondary sales;',
